b1da4ffb2ef354644482d1c5e63750e964ef0986
servady <servadyur@gmail.com>
2022-03-02 19:46:21 +0300

    added Protocol field in config

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -15,6 +15,7 @@ type config struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH"`
+	Protocol        string `env:"-"`
 }
 
 var cfg *config
@@ -24,13 +25,14 @@ func main() {
 	flag.StringVar(&cfg.ServerAddress, "a", "localhost:8080", "web server address")
 	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "base URL for shorten links")
 	flag.StringVar(&cfg.FileStoragePath, "f", "shorten.json", "shorten URL file path")
+	flag.StringVar(&cfg.Protocol, "p", "http", "protocol to use (http/https)")
 	flag.Parse()
 
 	if err := env.Parse(cfg); err != nil {
 		log.Fatal(err)
 	}
 	if !strings.Contains(cfg.BaseURL, cfg.ServerAddress) {
-		cfg.BaseURL = fmt.Sprintf("http://%s", cfg.ServerAddress)
+		cfg.BaseURL = fmt.Sprintf("%s://%s", cfg.Protocol, cfg.ServerAddress)
 	}
 	log.Println(cfg)
 	s, err := server.NewServer(cfg.ServerAddress, cfg.BaseURL, cfg.FileStoragePath)
