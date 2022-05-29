4bb7014058c192276032455b11d59a35dbd25db3
servady <servadyur@gmail.com>
2022-05-29 19:37:16 +0300

    added benchmarks and profiles

--- internal/pkg/handlers/handlers_test.go
+++ internal/pkg/handlers/handlers_test.go
@@ -18,6 +18,85 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestPostBatchHandler(t *testing.T) {
+	type want struct {
+		statusCode    int
+		contentType   string
+		generateURL   bool
+		urlRegex      *regexp.Regexp
+		correlationID string
+	}
+	tests := []struct {
+		name          string
+		request       string
+		longURL       []string
+		correlationID string
+		baseURL       string
+		want          want
+	}{
+		{
+			name:    "positive test #1",
+			request: "http://localhost:8080/api/shorten/batch",
+			longURL: []string{
+				"https://vk.com",
+				"https://youtube.com",
+				"https://google.com",
+				"https://gitlab.com",
+				"https://github.com",
+			},
+			correlationID: "12345",
+			baseURL:       "http://localhost:8080",
+			want: want{
+				statusCode:    201,
+				contentType:   "application/json",
+				generateURL:   true,
+				urlRegex:      regexp.MustCompile("http://localhost:8080/[a-z]{6}"),
+				correlationID: "12345",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			svc, err := service.NewService()
+			require.NoError(t, err)
+			h := handlers{
+				baseURL: tt.baseURL,
+				svc:     svc,
+			}
+			reqBody := make([]postBatchSingleRequest, 0)
+			for _, url := range tt.longURL {
+				reqBody = append(
+					reqBody,
+					postBatchSingleRequest{
+						CorrelationID: tt.correlationID,
+						OriginalURL:   url,
+					},
+				)
+			}
+			reqBz, err := json.Marshal(reqBody)
+			require.NoError(t, err)
+			request := httptest.NewRequest(http.MethodPost, tt.request, bytes.NewBuffer(reqBz))
+			request = request.WithContext(context.WithValue(request.Context(), contextKeyUID, uuid.New().String()))
+			w := httptest.NewRecorder()
+			hf := http.HandlerFunc(h.PostBatchHandler)
+			hf.ServeHTTP(w, request)
+			result := w.Result()
+
+			assert.Equal(t, tt.want.statusCode, result.StatusCode)
+			assert.Equal(t, tt.want.contentType, result.Header.Get("Content-Type"))
+			if tt.want.generateURL {
+				var res []postBatchSingleResponse
+				err := json.NewDecoder(result.Body).Decode(&res)
+				require.NoError(t, err)
+				err = result.Body.Close()
+				require.NoError(t, err)
+				assert.Regexp(t, tt.want.urlRegex, res[0].ShortURL)
+				assert.Equal(t, tt.want.correlationID, res[0].CorrelationID)
+			}
+		})
+	}
+}
+
 func Test_postURLApiHandler(t *testing.T) {
 	type want struct {
 		statusCode  int
@@ -252,3 +331,104 @@ func Test_getURLHandler(t *testing.T) {
 		})
 	}
 }
+
+func BenchmarkGetURLHandler(b *testing.B) {
+	svc, err := service.NewService()
+	if err != nil {
+		b.Errorf("unable to initiazlize service: %v\n", err)
+	}
+	h := handlers{
+		svc: svc,
+	}
+	uid := uuid.New().String()
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	h.svc.InsertNewURLPair(ctx, uid, "abcdef", "https://vk.com/groups")
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/abcdef", nil)
+	w := httptest.NewRecorder()
+	hf := http.HandlerFunc(h.GetURLHandler)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		hf.ServeHTTP(w, request)
+	}
+}
+
+func BenchmarkPostURLHandler(b *testing.B) {
+	svc, _ := service.NewService()
+	h := handlers{
+		baseURL: "http://localhost:8080",
+		svc:     svc,
+	}
+	request := httptest.NewRequest(
+		http.MethodPost,
+		"http://localhost:8080/",
+		strings.NewReader("https://gitlab.com/servady"),
+	)
+	request = request.WithContext(context.WithValue(request.Context(), contextKeyUID, uuid.New().String()))
+	w := httptest.NewRecorder()
+	hf := http.HandlerFunc(h.PostURLHandler)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		hf.ServeHTTP(w, request)
+	}
+}
+
+// func BenchmarkPostBatchHandler(b *testing.B) {
+// 	svc, err := service.NewService()
+// 	require.NoError(b, err)
+// 	h := handlers{
+// 		baseURL: "http://localhost:8080",
+// 		svc:     svc,
+// 	}
+// 	reqBody := []postBatchSingleRequest{
+// 		{CorrelationID: "12345", OriginalURL: "https://vk.com"},
+// 		{CorrelationID: "12345", OriginalURL: "https://google.com"},
+// 		{CorrelationID: "12345", OriginalURL: "https://youtube.com"},
+// 		{CorrelationID: "12345", OriginalURL: "https://gitlab.com"},
+// 		{CorrelationID: "12345", OriginalURL: "https://github.com"},
+// 	}
+// 	reqBz, err := json.Marshal(reqBody)
+// 	require.NoError(b, err)
+// 	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/api/shorten/batch", bytes.NewBuffer(reqBz))
+// 	w := httptest.NewRecorder()
+// 	hf := http.HandlerFunc(h.PostBatchHandler)
+
+// 	b.ReportAllocs()
+// 	b.ResetTimer()
+// 	for i := 0; i < b.N; i++ {
+// 		request = request.WithContext(context.WithValue(request.Context(), contextKeyUID, uuid.New().String()))
+// 		hf.ServeHTTP(w, request)
+// 	}
+// }
+
+// func BenchmarkPostURLApiHandler(b *testing.B) {
+// 	svc, _ := service.NewService()
+// 	h := handlers{
+// 		baseURL: "http://localhost:8080",
+// 		svc:     svc,
+// 	}
+// 	reqBody := postShortenRequest{
+// 		URL: "https://youtube.com/",
+// 	}
+// 	reqBz, err := json.Marshal(reqBody)
+// 	require.NoError(b, err)
+// 	request := httptest.NewRequest(
+// 		http.MethodPost,
+// 		"http://localhost:8080/api/shorten",
+// 		bytes.NewBuffer(reqBz),
+// 	)
+// 	request = request.WithContext(context.WithValue(request.Context(), contextKeyUID, uuid.New().String()))
+// 	w := httptest.NewRecorder()
+// 	// h := http.HandlerFunc(PostURLApiHandler)
+// 	hf := http.HandlerFunc(h.PostURLApiHandler)
+
+// 	b.ReportAllocs()
+// 	b.ResetTimer()
+// 	for i := 0; i < 2; i++ {
+// 		hf.ServeHTTP(w, request)
+// 	}
+// }
