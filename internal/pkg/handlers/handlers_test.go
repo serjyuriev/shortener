9fab9effb4a67abfd68d1837fdfa40db0d8d6071
servady <servadyur@gmail.com>
2022-06-06 22:52:30 +0300

    added examples for all handlers

--- internal/pkg/handlers/handlers_test.go
+++ internal/pkg/handlers/handlers_test.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -18,6 +20,382 @@ import (
 
 	"github.com/serjyuriev/shortener/internal/pkg/service"
 )
+
+func ExampleHandlers_DeleteURLsHandler() {
+	svc, err := service.NewService()
+	if err != nil {
+		fmt.Printf("unable to initiazlize service: %v\n", err)
+		return
+	}
+	h := &Handlers{svc: svc}
+
+	uid := uuid.New().String()
+	urls := map[string]string{
+		"abcdef": "https://github.com/serjyuriev",
+		"fedcba": "https://gitlab.com/servady",
+		"lkasdj": "https://yandex.ru",
+		"aslkqs": "https://google.com",
+		"cpsoks": "https://vk.com",
+		"qwrkml": "https://habr.com",
+		"sdfkbj": "https://discord.com",
+		"qlwknf": "https://gmail.com",
+		"zkljns": "https://twitch.tv",
+		"qkwnmd": "https://vscode.dev",
+	}
+	if err = h.svc.InsertManyURLs(context.Background(), uid, urls); err != nil {
+		fmt.Printf("unable to insert urls: %v\n", err)
+		return
+	}
+
+	toDelete := []string{
+		"qwrkml",
+		"fedcba",
+	}
+	reqBody, err := json.Marshal(toDelete)
+	if err != nil {
+		fmt.Printf("unable to marshal request body: %v\n", err)
+		return
+	}
+	request, err := http.NewRequest(
+		http.MethodDelete,
+		"http://localhost:8080/api/user/urls",
+		bytes.NewBuffer(reqBody),
+	)
+	if err != nil {
+		fmt.Printf("unable to initialize http request: %v\n", err)
+		return
+	}
+	request = request.WithContext(
+		context.WithValue(
+			request.Context(),
+			contextKeyUID,
+			uid,
+		),
+	)
+
+	w := httptest.NewRecorder()
+	hf := http.HandlerFunc(h.DeleteURLsHandler)
+	hf.ServeHTTP(w, request)
+	result := w.Result()
+	defer result.Body.Close()
+
+	fmt.Printf("Code: %d\n", result.StatusCode)
+
+	// Output:
+	// Code: 202
+}
+
+func ExampleHandlers_GetURLHandler() {
+	svc, err := service.NewService()
+	if err != nil {
+		fmt.Printf("unable to initiazlize service: %v\n", err)
+		return
+	}
+	h := &Handlers{svc: svc}
+
+	uid := uuid.New().String()
+	urls := map[string]string{
+		"abcdef": "https://github.com/serjyuriev",
+		"fedcba": "https://gitlab.com/servady",
+		"lkasdj": "https://yandex.ru",
+		"aslkqs": "https://google.com",
+		"cpsoks": "https://vk.com",
+		"qwrkml": "https://habr.com",
+		"sdfkbj": "https://discord.com",
+		"qlwknf": "https://gmail.com",
+		"zkljns": "https://twitch.tv",
+		"qkwnmd": "https://vscode.dev",
+	}
+	if err = h.svc.InsertManyURLs(context.Background(), uid, urls); err != nil {
+		fmt.Printf("unable to insert urls: %v\n", err)
+		return
+	}
+
+	request, err := http.NewRequest(
+		http.MethodGet,
+		"http://localhost:8080/zkljns",
+		nil,
+	)
+	if err != nil {
+		fmt.Printf("unable to initialize http request: %v\n", err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+	hf := http.HandlerFunc(h.GetURLHandler)
+	hf.ServeHTTP(w, request)
+	result := w.Result()
+	defer result.Body.Close()
+
+	fmt.Printf("Code: %d\n", result.StatusCode)
+	fmt.Printf("Original URL: %s\n", result.Header.Get("Location"))
+
+	// Output:
+	// Code: 307
+	// Original URL: https://twitch.tv
+}
+
+func ExampleHandlers_GetUserURLsAPIHandler() {
+	svc, err := service.NewService()
+	if err != nil {
+		fmt.Printf("unable to initiazlize service: %v\n", err)
+		return
+	}
+	h := &Handlers{svc: svc}
+
+	uid := uuid.New().String()
+	urls := map[string]string{
+		"abcdef": "https://github.com/serjyuriev",
+		"fedcba": "https://gitlab.com/servady",
+		"lkasdj": "https://yandex.ru",
+		"aslkqs": "https://google.com",
+		"cpsoks": "https://vk.com",
+	}
+
+	uid2 := uuid.New().String()
+	urls2 := map[string]string{
+		"qwrkml": "https://habr.com",
+		"sdfkbj": "https://discord.com",
+		"qlwknf": "https://gmail.com",
+		"zkljns": "https://twitch.tv",
+		"qkwnmd": "https://vscode.dev",
+	}
+	if err = h.svc.InsertManyURLs(context.Background(), uid, urls); err != nil {
+		fmt.Printf("unable to insert urls: %v\n", err)
+		return
+	}
+	if err = h.svc.InsertManyURLs(context.Background(), uid2, urls2); err != nil {
+		fmt.Printf("unable to insert urls: %v\n", err)
+		return
+	}
+
+	request, err := http.NewRequest(
+		http.MethodGet,
+		"http://localhost:8080/api/user/urls",
+		nil,
+	)
+	if err != nil {
+		fmt.Printf("unable to initialize http request: %v\n", err)
+		return
+	}
+	request = request.WithContext(
+		context.WithValue(
+			request.Context(),
+			contextKeyUID,
+			uid,
+		),
+	)
+
+	w := httptest.NewRecorder()
+	hf := http.HandlerFunc(h.GetUserURLsAPIHandler)
+	hf.ServeHTTP(w, request)
+	result := w.Result()
+	defer result.Body.Close()
+
+	var firstUserURLs []userURLs
+	err = json.NewDecoder(result.Body).Decode(&firstUserURLs)
+
+	fmt.Printf("Code: %d\n", result.StatusCode)
+	fmt.Printf("URLs found: %d\n", len(firstUserURLs))
+
+	// Output:
+	// Code: 200
+	// URLs found: 5
+}
+
+func ExampleHandlers_PingHandler() {
+	svc, err := service.NewService()
+	if err != nil {
+		fmt.Printf("unable to initiazlize service: %v\n", err)
+		return
+	}
+	h := &Handlers{svc: svc}
+
+	request, err := http.NewRequest(
+		http.MethodGet,
+		"http://localhost:8080/ping",
+		nil,
+	)
+	if err != nil {
+		fmt.Printf("unable to initialize http request: %v\n", err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+	hf := http.HandlerFunc(h.PingHandler)
+	hf.ServeHTTP(w, request)
+	result := w.Result()
+	defer result.Body.Close()
+
+	body, err := io.ReadAll(result.Body)
+	if err != nil {
+		fmt.Printf("unable to read response body: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Code: %d\n", result.StatusCode)
+	fmt.Printf("Response: %s\n", string(body))
+
+	// Output:
+	// Code: 200
+	// Response: ok
+}
+
+func ExampleHandlers_PostBatchHandler() {
+	svc, err := service.NewService()
+	if err != nil {
+		fmt.Printf("unable to initiazlize service: %v\n", err)
+		return
+	}
+	h := &Handlers{svc: svc}
+
+	uid := uuid.New().String()
+	reqArray := []postBatchSingleRequest{
+		{
+			CorrelationID: "12345",
+			OriginalURL:   "https://github.com/serjyuriev",
+		},
+		{
+			CorrelationID: "12345",
+			OriginalURL:   "https://gitlab.com/servady",
+		},
+		{
+			CorrelationID: "12345",
+			OriginalURL:   "https://google.com",
+		},
+		{
+			CorrelationID: "12345",
+			OriginalURL:   "https://twitch.tv",
+		},
+		{
+			CorrelationID: "12345",
+			OriginalURL:   "https://habr.com",
+		},
+	}
+	reqBody, err := json.Marshal(reqArray)
+	if err != nil {
+		fmt.Printf("unable to marshal request body: %v\n", err)
+		return
+	}
+	request, err := http.NewRequest(
+		http.MethodPost,
+		"http://localhost:8080/api/shorten/batch",
+		bytes.NewBuffer(reqBody),
+	)
+	if err != nil {
+		fmt.Printf("unable to initialize http request: %v\n", err)
+		return
+	}
+	request = request.WithContext(
+		context.WithValue(
+			request.Context(),
+			contextKeyUID,
+			uid,
+		),
+	)
+
+	w := httptest.NewRecorder()
+	hf := http.HandlerFunc(h.PostBatchHandler)
+	hf.ServeHTTP(w, request)
+	result := w.Result()
+	defer result.Body.Close()
+
+	var response []postBatchSingleResponse
+	if err = json.NewDecoder(result.Body).Decode(&response); err != nil {
+		fmt.Printf("unable to decode response body: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Code: %d\n", result.StatusCode)
+	fmt.Printf("Added URLs: %d\n", len(response))
+
+	// Output:
+	// Code: 201
+	// Added URLs: 5
+}
+
+func ExampleHandlers_PostURLApiHandler() {
+	svc, err := service.NewService()
+	if err != nil {
+		fmt.Printf("unable to initiazlize service: %v\n", err)
+		return
+	}
+	h := &Handlers{svc: svc}
+
+	uid := uuid.New().String()
+	req := postShortenRequest{URL: "https://twitch.tv"}
+	reqBody, err := json.Marshal(req)
+	if err != nil {
+		fmt.Printf("unable to marshal request body: %v\n", err)
+		return
+	}
+	request, err := http.NewRequest(
+		http.MethodPost,
+		"http://localhost:8080/api/shorten",
+		bytes.NewBuffer(reqBody),
+	)
+	if err != nil {
+		fmt.Printf("unable to initialize http request: %v\n", err)
+		return
+	}
+	request = request.WithContext(
+		context.WithValue(
+			request.Context(),
+			contextKeyUID,
+			uid,
+		),
+	)
+
+	w := httptest.NewRecorder()
+	hf := http.HandlerFunc(h.PostURLApiHandler)
+	hf.ServeHTTP(w, request)
+	result := w.Result()
+	defer result.Body.Close()
+
+	fmt.Printf("Code: %d\n", result.StatusCode)
+
+	// Output:
+	// Code: 201
+}
+
+func ExampleHandlers_PostURLHandler() {
+	svc, err := service.NewService()
+	if err != nil {
+		fmt.Printf("unable to initiazlize service: %v\n", err)
+		return
+	}
+	h := &Handlers{svc: svc}
+
+	uid := uuid.New().String()
+	url := []byte("https://github.com/serjyuriev")
+	request, err := http.NewRequest(
+		http.MethodPost,
+		"http://localhost:8080/",
+		bytes.NewBuffer(url),
+	)
+	if err != nil {
+		fmt.Printf("unable to initialize http request: %v\n", err)
+		return
+	}
+	request = request.WithContext(
+		context.WithValue(
+			request.Context(),
+			contextKeyUID,
+			uid,
+		),
+	)
+
+	w := httptest.NewRecorder()
+	hf := http.HandlerFunc(h.PostURLHandler)
+	hf.ServeHTTP(w, request)
+	result := w.Result()
+	defer result.Body.Close()
+
+	fmt.Printf("Code: %d\n", result.StatusCode)
+
+	// Output:
+	// Code: 201
+}
 
 func TestPostBatchHandler(t *testing.T) {
 	type want struct {
